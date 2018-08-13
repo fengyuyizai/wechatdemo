2c251c17307eb13fd5670ba79db3e771b1221a94
1942316144@qq.com <1942316144@qq.com>
2018-08-13 10:20:45 +0800

    '完善'

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -17,31 +17,39 @@ Page({
     
   },
   getUserInfo: function(e) {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
     if (app.globalData.userInfo) {
+      wx.hideLoading()
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+      console.log('1');
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
+      
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
+          wx.hideLoading()
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
         }
       })
+      console.log('3');
     }
+  },
+  removeUserInfo: function (e) {
+    app.globalData.userInfo = null;
+    this.setData({
+      userInfo: {},
+      hasUserInfo: false
+    })
+    console.log('removeUserInfo' + this.userInfo)
   }
 })
